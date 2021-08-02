1d6b906e73b64b329b5aaa2a58d6ab81b50d09ab
Simone Magnani <magnani.sim@gmail.com>
2021-08-02 11:41:23 +0100

    Add functions to read all users and one user from DB

---
+++ controllers/landing.js
@@ -0,0 +1,22 @@
+
+const models = require('../models');
+
+exports.get_landing = function(req, res, next) {
+    res.render('landing', { title: 'Express' });
+}
+
+exports.show_users = function(req, res, next) {
+    return models.Users.findAll().then(users => {
+        res.render('landing', { title: 'Express', users: users});
+    })
+}
+
+exports.show_user = function(req, res, next) {
+    return models.Users.findOne({
+        where : {
+            id : req.params.user_id
+        }
+    }).then(user => {
+        res.render('user', { user : user });
+    });
+}
