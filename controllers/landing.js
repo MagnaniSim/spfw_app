635c3bf4876d578502043dbd07d3df73703592eb
Simone Magnani <magnani.sim@gmail.com>
2021-08-14 12:25:39 +0100

    Changing Professionals UserId Column name, adding joint tables search

--- controllers/landing.js
+++ controllers/landing.js
@@ -35,7 +35,7 @@ exports.get_landing = function(req, res, next) {
 
 const rerender_landing = function(errors, req, res, next) {
     get_pro_details(req,res,next);
-    res.render('landing', {user: req.user, formData: req.body, errors: errors});
+    res.render('landing', {user: req.user, formData: req.body, errors: errors, userData: req.userData});
 }
 
 exports.show_users = function(req, res, next) {
