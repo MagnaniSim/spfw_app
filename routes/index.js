c5122d753ff123c1e58ae32fa1c63c965f09b7aa
u3sb <u3sb@os>
2021-08-14 18:32:19 +0100

    Adding user editing

--- routes/index.js
+++ routes/index.js
@@ -19,5 +19,12 @@ router.get('/results', auth.is_logged_in, results.show_results);
 router.get('/', auth.is_logged_in, landing.get_landing);
 router.post('/', auth.is_logged_in, landing.search);
 
+/* User administration*/
+router.get('/users', auth.is_admin, user.show_users);
+router.get('/user/:user_id', auth.is_admin, user.show_user);
+router.get('/user/:user_id/edit', auth.is_admin, user.show_edit_user);
+router.post('/user/:user_id/edit', auth.is_admin, user.edit_user);
+router.post('/user/:user_id/delete', auth.is_admin, user.delete_user);
+router.post('/user/:user_id/delete-json', auth.is_admin, user.delete_user_json)
 
 module.exports = router;
