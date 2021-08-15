baf970a0f112e7eebab371a384359c73baef1d00
u3sb <u3sb@os>
2021-08-15 16:11:34 +0100

    Adding warn and info logs on admin access

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 let createError = require('http-errors')
 const models = require("../models");
+const logger = require("../util/logger");
 
 exports.is_logged_in = function(req, res, next) {
     if (!req.user) {
@@ -10,19 +11,25 @@ exports.is_logged_in = function(req, res, next) {
 }
 
 exports.is_admin = function(req, res, next) {
-    if (!req.user || !req.user.is_admin) {
+    if(!req.user){
+        logger.warn("Invalid admin access attempt: id: unknown");
         res.redirect('/');
+    } else if(!req.user.is_admin){
+            logger.warn("Invalid admin access attempt: id: " + req.user.id);
+            res.redirect('/');
     } else {
         // confirm user is an administrator
         let user;
         user = models.Users.findOne({
             where: {
-                id: req.params.user_id
+                id: req.user.id
             }
         })
         if (!user && !user.is_admin) {
+            logger.warn("Invalid admin access attempt: id: " + req.user.id);
             res.redirect('/');
         } else {
+            logger.info("Admin access: id: " + req.user.id);
             next();
         }
     }
